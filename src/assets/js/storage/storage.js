72a69c49006f497a0cad0c5dc722f99863f55395
Kornull <wolod1987kor@gmail.com>
2022-05-07 11:33:30 +0300

    feat: add storage file

---
+++ src/assets/js/storage/storage.js
@@ -0,0 +1,9 @@
+export function set(name, lang) {
+  localStorage.setItem(name, JSON.stringify(lang));
+}
+
+export function get(name) {
+  return JSON.parse(localStorage.getItem(name));
+}
+
+set('keyLang', 'en');
