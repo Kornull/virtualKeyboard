1e83d8a081f0544a84ee16b9e62fbbee4227e599
Kornull <wolod1987kor@gmail.com>
2022-05-08 14:52:02 +0300

    feat: add event pre-reading

--- src/assets/js/classes/Keyboard.js
+++ src/assets/js/classes/Keyboard.js
@@ -6,8 +6,10 @@ export default class Keyboard {
     this.lang = lang;
     this.main = document.createElement('div');
     this.main.classList.add('main');
-    this.keyboard = null;
+    this.keyboard = document.createElement('div');
+    this.keyboard.classList.add('keyboard');
     this.key = null;
+    this.case = null;
   }
 
   addInput() {
@@ -19,11 +21,13 @@ export default class Keyboard {
   }
 
   init(lang) {
+    // new keyboard
+    this.keyboard.innerHTML = '';
+    this.btns = [];
+
     this.lang = lang;
     let count = 0;
     let coutArrou = 0;
-    this.keyboard = document.createElement('div');
-    this.keyboard.classList.add('keyboard');
 
     this.key = document.createElement('div');
     this.key.classList.add('keyboards__key');
@@ -31,16 +35,18 @@ export default class Keyboard {
     const arrows = document.createElement('div');
     arrows.classList.add('arrows');
 
-    this.keys.forEach((element) => {
+    this.keys.forEach((el) => {
       const rows = [];
       const row = document.createElement('div');
       row.classList.add('row', `row-${count += 1}`);
-      element.forEach((x) => {
+      el.forEach((x) => {
+        // create btn
         language[this.lang].forEach((i) => {
           if (i.keyCode === x) {
             this.key = document.createElement('div');
             this.key.classList.add('keyboards__key');
             this.key.id = i.keyCode.toLowerCase();
+            // add style func btn
             if (this.key.id === 'space') { this.key.style.width = '510px'; }
             if (this.key.id === 'capslock') { this.key.classList.add('keyboards__key--caps'); }
             if (this.key.id.match(/backspace|shift|enter/)) { this.key.style.width = '180px'; }
@@ -55,8 +61,14 @@ export default class Keyboard {
               arrow.classList.add(`arrou-${coutArrou += 1}`);
               arrow.append(this.key);
               arrows.appendChild(arrow);
+
+              // collect an array btns
+              this.btns.push(this.key);
               rows.push(this.key.id);
             } else {
+              // collect an array btns
+              this.btns.push(this.key);
+
               this.key.innerHTML = i.small;
               row.appendChild(this.key);
               row.appendChild(arrows);
@@ -70,5 +82,34 @@ export default class Keyboard {
       row.style.gridTemplateColumns = `repeat(${rows.length}, 1fr)`;
     });
     this.main.appendChild(this.keyboard);
+  }
+
+  addRunKey(ev) {
+    this.text.focus();
+    if (ev.stopPropaganation) ev.stopPropaganation();
+    const { code, type } = ev;
+
+    this.btns.forEach((x) => {
+      // checking for btns
+      if (x.id === code.toLowerCase()) {
+        if (type.match(/keydown|mousedown/)) {
+          if (type.match(/key/)) {
+            ev.preventDefault();
+
+            // write to textarrea
+            const a = language[this.lang].find((el) => el.keyCode.toLowerCase() === x.id);
+            x.classList.add('active');
+            if (x.id.match(/key|digc|bracket|slash|comma|period|quote|semi/)) this.text.value += a.small;
+            if (x.id.match(/tab/)) this.text.value += '    ';
+            if (x.id.match(/ent/)) this.text.value += '\n';
+            if (x.id.match(/shift/)) {
+              this.init('ru');
+            }
+          }
+        } else if (type.match(/keyup/)) {
+          x.classList.remove('active');
+        }
+      }
+    });
   }
 }
